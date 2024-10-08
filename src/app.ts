442ae41e256a3bb99b127202f377b2fa57d911d5
Jamie Evans <jamie.evans@l1.com>
2024-10-08 11:53:52 +0100

    Added env entry and db connection

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import { AppDataSource } from './config/ormconfig';
+import express from 'express';
+import session from 'express-session';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = process.env.APP_PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(session
+    ({
+        secret: 'my_secret',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: 'auto' }
+    })
+);
+
+// Initialize TypeORM and start the server
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Data Source has been initialized!');
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    })
+    .catch((error) => console.error('Error during Data Source initialization:', error));
---
+++ src/config/ormconfig.ts
@@ -0,0 +1,11 @@
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT || '5432'), // Convert to number
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    logging: true,
+})
